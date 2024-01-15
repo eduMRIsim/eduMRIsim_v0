c0dd9179d44b8cf2a4da64ce9a2ac1377b749bc2
eduMRIsim <s.e.g.r.gonzalez.riedel@tue.nl>
2024-01-15 15:01:29 +0100

    Refactor Model class constructor and property names

--- model.py
+++ model.py
@@ -1,18 +1,18 @@
 class Model:
-    def __init__(self, model_name, description, T1map, T2map, PDmap):
-        self._model_name = model_name
+    def __init__(self, name, description, T1map, T2map, PDmap):
+        self._name = name
         self._description = description
         self._T1map = T1map
         self._T2map = T2map
         self._PDmap = PDmap 
 
     @property
-    def model_name(self):
-        return self._model_name
+    def name(self):
+        return self._name
     
-    @model_name.setter
-    def model_name(self, name):
-        self._model_name = name 
+    @name.setter
+    def name(self, name):
+        self._name = name 
 
     @property
     def T1map(self):
