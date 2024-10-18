9b3ecd238f625fc8efb8db59a33c6fa0a2678d0d
M-L-Red <m.f.p.lenders@student.tue.nl>
2024-10-18 22:32:21 +0200

    Add existing shortcuts for distance measuring, zooming, and panning, to the helping window

--- views/ui/shortcut_dialog_ui.py
+++ views/ui/shortcut_dialog_ui.py
@@ -23,6 +23,24 @@ class ShortcutDialog(QDialog):
             ("Ctrl+W", "Toggle window leveling"),
             ("Ctrl+A", "Measure area"),
             ("Ctrl+H", "Show keyboard shortcuts (this dialog)"),
+            ("M", "Toggle distance measuring:\n"
+                  "1. Left click into a planning view window.\n"
+                  "2. Move the mouse to one of the two end points of the line to be measured.\n"
+                  "3. Press M to start measuring from the mouse cursor.\n"
+                  "4. Move the mouse to the other end point of the line to be measured.\n"
+                  "5. Press M again to stop measuring"),
+            ("Z", "Toggle zooming:\n"
+                  "1. Left click outside of the planning view window that the image that you want to zoom is in, but still within the app window.\n"
+                  "2. Move the mouse into the planning view window, over the image.\n"
+                  "3. Hold left click and Z to start zooming.\n"
+                  "4. While holding left click and Z, move the mouse up and down to zoom in and out.\n"
+                  "5. Release Z to stop zooming"),
+            ("P", "Toggle panning:\n"
+                  "1. Left click outside of the planning view window that the image that you want to pan is in, but still within the app window.\n"
+                  "2. Move the mouse into the planning view window, over the image.\n"
+                  "3. Hold left click and P to start panning.\n"
+                  "4. While holding left click and P, move the mouse in any direction to pan the image.\n"
+                  "5. Release P to stop panning"),
         ]
 
         # Create a table to display the shortcuts
@@ -47,6 +65,10 @@ class ShortcutDialog(QDialog):
         header.setDefaultAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
 
         # Resize the table to fit its contents, and add padding
+        for row in range(table.rowCount()):
+            table.setRowHeight(row, table.rowHeight(row) + 20)
+            if row == 6 or row == 7 or row == 8:
+                table.setRowHeight(row, table.rowHeight(row) + 50)
         table.resizeColumnsToContents()
         for column in range(table.columnCount()):
             table.setColumnWidth(column, table.columnWidth(column) + 20)  # Adding extra 20 pixels
