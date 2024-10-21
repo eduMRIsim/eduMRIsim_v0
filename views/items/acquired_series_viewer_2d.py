1459ebdcb10e5c2422e6348994b0ae7de800ea5c
M-L-Red <m.f.p.lenders@student.tue.nl>
2024-10-21 19:41:49 +0200

    Fix middle point coloring (is now no longer filled, and red for inactive stacks)

--- views/items/acquired_series_viewer_2d.py
+++ views/items/acquired_series_viewer_2d.py
@@ -882,6 +882,8 @@ class StackItem:
         self.middle_line_display = MiddleLineItem(pixmap_item)
         self.stack_index = stack_index
 
+        self.volume_display.middle_point.setPen(Qt.GlobalColor.red)
+
     # clear all the visual elements connected to this stack before removing this stack item
     def __del__(self):
         self.clear_objects()
@@ -894,6 +896,8 @@ class StackItem:
         self.middle_line_display.setVisible(True)
         self.volume_display.set_handles_visible()
         self.volume_display.is_active_stack = True
+
+        self.volume_display.middle_point.setPen(Qt.GlobalColor.yellow)
 
     # unselect this scan volume so set it red and make non-movable
     def set_inactive_settings(self):
@@ -908,6 +912,8 @@ class StackItem:
         self.slices_display = []
         self.volume_display.set_handles_invisible()
         self.volume_display.is_active_stack = False
+
+        self.volume_display.middle_point.setPen(Qt.GlobalColor.red)
 
     # update stack item display component with coordinates
     def update_objects_with_pixmap_coords(
